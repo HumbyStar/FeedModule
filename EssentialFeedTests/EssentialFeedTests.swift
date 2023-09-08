5bf0f3846736d9b8ab874e8e4f32fcd3b76b3a59
Humberto <betogrt300@gmail.com>
2023-09-08 15:06:41 -0300

    removed high hidden from HTTPClient with RemoteFeedLoader

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -9,23 +9,30 @@ import XCTest
 @testable import EssentialFeed
 
 class RemoteFeedLoader {
+    let client: HTTPClient
+    let url: URL
+    
+    init(client: HTTPClient, url: URL = (URL(string:  "https://a-URL.com.br")!)) {
+        self.client = client
+        self.url = url
+    }
+    
     func load() {
-        HTTPClient.shared.getURL(url: URL(string: "https://a-URL.com.br")!)
+        client.getURL(url: url)
         
         //Responsabilidade do RemoteFeedLoader chamar o método getURL da HTTPClient
         //Responsabilidade do RemoteFeedLoader localizar o HTTPClient na memória, isso os torna acoplados.
     }
 }           // Precisa ter alguma URL em algum momento
 
-class HTTPClient {
-    static var shared = HTTPClient()
-    func getURL(url: URL) {}
+protocol HTTPClient {
+    func getURL(url: URL)
 }
 
 class HTTPClientSpy: HTTPClient {
     var requestURL: URL?                    // RequestURL é apenas para fim de testes (produção)
     
-    override func getURL(url: URL) {
+    func getURL(url: URL) {
         requestURL = url
     }
 }
@@ -34,22 +41,22 @@ final class RemoteFeedTests: XCTestCase {
 
     func tests_init_withoutURLRequest() {
         //let sut = RemoteFeedLoader() --------> Mas não faz sentido eu possui-lo porque eu não o uso
+        
         let client = HTTPClientSpy()
-        
-        HTTPClient.shared = client
-        
         XCTAssertNil(client.requestURL)
     }
     
     func tests_init_withRequestURL() {
-        let sut = RemoteFeedLoader()
+        let url = URL(string: "https://possibilidadesDeOutroLink.com.br")!
         let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client, url: url)
         
-        HTTPClient.shared = client
+        
+//        let url = URL(string: "https://possibilidadesDeOutroLink.com.br")!
         
         sut.load()
         
-        XCTAssertNotNil(client.requestURL)
+        XCTAssertEqual(client.requestURL, url)
     }
 
 }
