52f7f90b22adf46cd66db71b625198d745c2c3eb
Humberto <betogrt300@gmail.com>
2023-09-08 13:22:07 -0300

    create a singleton to comunicate the url with sut and client through load function

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -8,29 +8,36 @@
 import XCTest
 @testable import EssentialFeed
 
-final class EssentialFeedTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.requestURL = "https://a-URL.com.br"
     }
+}           // Precisa ter alguma URL em algum momento
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+class HTTPClient {
+    static let shared = HTTPClient()
+    var requestURL: String?
+    
+    private init(){}
+    
+}
+
+final class RemoteFeedTests: XCTestCase {
+
+    func tests_init_withoutURLRequest() {
+        //let sut = RemoteFeedLoader() --------> Mas não faz sentido eu possui-lo porque eu não o uso
+        let client = HTTPClient.shared
+        
+        XCTAssertNil(client.requestURL)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func tests_init_withRequestURL() {
+        let sut = RemoteFeedLoader()
+        let client = HTTPClient.shared
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestURL)
     }
 
 }
