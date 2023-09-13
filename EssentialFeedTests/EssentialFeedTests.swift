902515c9383431cc06c73803cb0f90f296549fd3
Humberto <betogrt300@gmail.com>
2023-09-13 19:11:31 -0300

    Capturing error on test and passing it to RemoteFeedTests that handle the specify error on test to RemoteFeedLoader

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -9,19 +9,27 @@ import XCTest
 import EssentialFeed
 
 public protocol HTTPClient {
-    func get(url: URL)
+    func get(url: URL, completion: @escaping (Error) -> Void)
 }
 
 public final class RemoteFeedLoader {
     private let client: HTTPClient
     private let url: URL
     
+    public enum Error {
+        case connectivity
+    }
+    
     public init(client: HTTPClient, url: URL) {
         self.client = client
         self.url = url
     }
     
-    public func load() {}
+    public func load(completion: @escaping (Error) -> Void = {_ in}) {
+        client.get(url: url) { error in
+            completion(.connectivity)
+        }
+    }
 }
 
 
@@ -51,6 +59,18 @@ final class RemoteFeedTests: XCTestCase {
         XCTAssertEqual(client.requestURLs, [url,url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        
+        client.error = NSError(domain: "test", code: 0)
+        
+        var capturedError: RemoteFeedLoader.Error?
+        sut.load { error in capturedError = error }
+        
+        XCTAssertEqual(capturedError, .connectivity)
+        
+    }
+    
     func makeSUT(url: URL = URL(string:  "https://a-URL.com.br")!) -> (RemoteFeedLoader,HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(client: client, url: url)
@@ -59,9 +79,14 @@ final class RemoteFeedTests: XCTestCase {
     
     class HTTPClientSpy: HTTPClient {
         var requestURLs = [URL]()
+        var error: Error?
         
-        func get(url: URL) {
+        func get(url: URL, completion: (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestURLs.append(url)
         }
+        
     }
 }
